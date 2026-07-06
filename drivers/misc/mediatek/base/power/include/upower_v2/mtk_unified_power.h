17cb455eafdfb0ecfe8e81e26c4fbc80d355f613
Youff <asa.jazal@gmail.com>
2026-07-06 09:10:39 +0000

    port: upower_v2 (Unified Power Model) from 4.19
    Add Unified Power Model (UPower v2) infrastructure for MT6785. This
    provides power tables, capacity data, and energy estimation used by
    the CPU frequency governor and scheduler.
    
    Changes from 4.19:
    - proc_ops migration: PROC_FOPS_RW/PROC_FOPS_RO macros converted
      from struct file_operations to struct proc_ops (dropped .owner,
      renamed .open/.read/.llseek/.release/.write to
      .proc_open/.proc_read/.proc_lseek/.proc_release/.proc_write)
    - do_gettimeofday() removed: UPOWER_PROFILE_API_TIME profiling
      now uses ktime_get_real() instead
    - Kconfig entry added to mediatek Power menu
    - Build wiring: main Makefile references base/power/upower_v2/
    - Platform Makefile uses MTK_PLATFORM pattern from EEM
    
    Files added:
    - base/power/include/mtk_upower.h: wrapper header
    - base/power/include/upower_v2/mtk_unified_power.h: redirect to
      include/mt-plat/upower_v2/ (uses 5.10 tree layout)
    - base/power/include/upower_v2/mtk_unified_power_data.h: same
    - base/power/upower_v2/: 4 source files + Makefile
      (mtk_unified_power.c, mtk_unified_power_api.c,
       mtk_unified_power_internal.c, mtk_unified_power_mt6785.c)
    - defconfig: CONFIG_MTK_UNIFY_POWER=y enabled
    
    Signed-off-by: Youffx <asa.jazal@gmail.com>

---
+++ drivers/misc/mediatek/base/power/include/upower_v2/mtk_unified_power.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef MTK_UNIFIED_POWER_H
+#define MTK_UNIFIED_POWER_H
+
+#if defined(CONFIG_MTK_UNIFY_POWER)
+#include "../../../../mt-plat/upower_v2/mtk_unified_power.h"
+#endif
+
+#endif
---
+++ drivers/misc/mediatek/base/power/include/upower_v2/mtk_unified_power_data.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+
+#ifndef MTK_UNIFIED_POWER_DATA_H
+#define MTK_UNIFIED_POWER_DATA_H
+
+#if defined(CONFIG_MTK_UNIFY_POWER)
+#include "../../../../mt-plat/upower_v2/mtk_unified_power_data.h"
+#endif
+
+#endif
